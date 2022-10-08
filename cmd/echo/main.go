bd1ca4172352b8ecc228ba9af6d9bc4850f38037
mkaiho <mkaiho.cgengo@gmail.com>
2022-10-08 02:18:58 +0000

    add command

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/mkaiho/go-ecs-batch-sample/logging"
+	"github.com/spf13/cobra"
+)
+
+var (
+	command *cobra.Command
 )
 
 func init() {
 	logging.InitLoggerWithZap()
+	command = newCommand()
 }
 
 func main() {
-	logging.GetLogger().Info("not implemented")
+	if err := command.Execute(); err != nil {
+		logging.GetLogger().Error(err, "error has occured")
+	}
+}
+
+func newCommand() *cobra.Command {
+	command := cobra.Command{
+		Use:   "echo args...",
+		Short: "display args",
+		Long:  "Display arguments on stdout.",
+		RunE:  handle,
+	}
+
+	return &command
+}
+
+func handle(cmd *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		logging.GetLogger().Info("no args")
+	} else {
+		logging.GetLogger().Info(strings.Join(args, " "))
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.19
 require (
 	github.com/go-logr/logr v1.2.3
 	github.com/go-logr/zapr v1.2.3
+	github.com/spf13/cobra v1.5.0
 	go.uber.org/zap v1.23.0
 )
 
 require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
